232441065c242e063237ebc8b290ac6575d3bdd1
takhmadiev <davidovskiy88@gmail.com>
2023-09-29 11:22:55 +0500

    Fixing tests in test_homework, adding __pycache to gitignore

--- test_homework.py
+++ test_homework.py
@@ -1,3 +1,7 @@
+import math
+
+
+PI = math.pi
 
 
 def test_greeting():
@@ -7,7 +11,7 @@ def test_greeting():
     name = "Анна"
     age = 25
     # TODO Сформируйте нужную строку
-    output = ""
+    output = f"Привет, {name}! Тебе {age} лет."
     # Проверяем результат
     assert output == "Привет, Анна! Тебе 25 лет."
 
@@ -21,11 +25,11 @@ def test_rectangle():
     b = 20
 
     # TODO сосчитайте периметр
-    perimeter = 0
+    perimeter = a*2+b*2
     assert perimeter == 60
 
     # TODO сосчитайте площадь
-    area = 0
+    area = a*b
     assert area == 200
 
 
@@ -36,11 +40,12 @@ def test_circle():
     """
     r = 23
     # TODO сосчитайте площадь
-    area = 0
+    area = PI*(r**2)
+    print(area)
     assert area == 1661.9025137490005
 
     # TODO сосчитайте длину окружности
-    length = 0
+    length = 2*PI*r
     assert length == 144.51326206513048
 
 
@@ -50,7 +55,8 @@ def test_random_list():
     """
 
     # TODO создайте список
-    l = []
+    l = [3, 90, 67, 53, 83, 22, 31, 9, 99, 76]
+    l.sort()
     assert len(l) == 10
     assert l[0] < l[-1]
 
@@ -61,7 +67,7 @@ def test_unique_elements():
     """
     l = [1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 8, 9, 10, 10]
     # TODO удалите повторяющиеся элементы
-
+    l = list(set(l))
     assert isinstance(l, list)
     assert len(l) == 10
     assert l == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
@@ -78,6 +84,6 @@ def test_dicts():
     second = [1, 2, 3, 4, 5]
     # TODO создайте словарь
     d = {}
-
+    d = dict(zip(first, second))
     assert isinstance(d, dict)
     assert len(d) == 5
